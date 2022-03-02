449f776d6bb908b72f7b543c1e45f9cefab2c9e3
BenGallardBridger <60215170+BenGallardBridger@users.noreply.github.com>
2022-03-02 00:44:43 +0000

    removes unnecessary imports

--- MedicationReminderApplication/app/src/main/java/com/example/medicationreminderapplication/mainPage.java
+++ MedicationReminderApplication/app/src/main/java/com/example/medicationreminderapplication/mainPage.java
@@ -1,6 +1,5 @@
 package com.example.medicationreminderapplication;
 
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,8 +18,8 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.pm.PermissionGroupInfo;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
