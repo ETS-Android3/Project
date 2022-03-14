bb6abca236722ef1c49e41da6de98104c1859eaf
BenGallardBridger <60215170+BenGallardBridger@users.noreply.github.com>
2022-03-14 15:18:57 +0000

    Page to start when notification is clicked

---
+++ MedicationReminderApplication/app/src/main/java/com/example/medicationreminderapplication/loginNotification.java
@@ -0,0 +1,33 @@
+package com.example.medicationreminderapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.KeyguardManager;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class loginNotification extends AppCompatActivity {
+    int LOCK_REQUEST_CODE = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_notification);
+        KeyguardManager kgm = (KeyguardManager) getSystemService(KEYGUARD_SERVICE);
+        Intent screenLockIntent = kgm.createConfirmDeviceCredentialIntent("", "");
+
+        startActivityForResult(screenLockIntent, LOCK_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(LOCK_REQUEST_CODE == requestCode){
+            if (resultCode == RESULT_OK) {
+                Intent intent = new Intent(loginNotification.this, mainPage.class);
+                startActivity(intent);
+            } else {
+                //Authentication failed
+            }
+        }
+    }
+}
